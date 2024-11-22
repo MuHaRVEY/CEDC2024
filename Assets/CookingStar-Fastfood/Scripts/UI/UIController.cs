78155c7cdb263e7a92929366d7fdac3249f3d3c0
jangjihyee <wgw009933@gmail.com>
2024-11-23 01:07:43 +0900

    재료 플레이트에 놓기

---
+++ Assets/CookingStar-Fastfood/Scripts/UI/UIController.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace CookingStar
+{
+    public class UIController : MonoBehaviour
+    {
+        public static UIController instance;
+
+        private void Awake()
+        {
+            instance = this;
+        }
+        //패킹씬 이동 추가
+        public void LoadPackingScene()
+        {
+        SfxPlayer.instance.PlaySfx(0);
+        StartCoroutine(LoadSceneDelayed("Packing"));
+        }
+
+        public void LoadShop()
+        {
+            SfxPlayer.instance.PlaySfx(0);
+            StartCoroutine(LoadSceneDelayed("Shop"));
+        }
+
+        public void LoadCareerMode()
+        {
+            SfxPlayer.instance.PlaySfx(0);
+            PlayerPrefs.SetString("gameMode", "CAREER");
+            PlayerPrefs.SetInt("IsTutorial", 0);
+            StartCoroutine(LoadSceneDelayed("LevelSelection"));
+        }
+
+        public void LoadFreePlay()
+        {
+            SfxPlayer.instance.PlaySfx(0);
+            PlayerPrefs.SetString("gameMode", "FREEPLAY");
+            PlayerPrefs.SetInt("IsTutorial", 0);
+            StartCoroutine(LoadSceneDelayed("Game"));
+        }
+
+        public void LoadTutorial()
+        {
+            SfxPlayer.instance.PlaySfx(0);
+            PlayerPrefs.SetString("gameMode", "FREEPLAY");
+            PlayerPrefs.SetInt("AvailableCandy", PlayerPrefs.GetInt("AvailableCandy", 0) + 3);  //Add +3 prize candy for tutorial
+            PlayerPrefs.SetInt("IsTutorial", 1);
+            StartCoroutine(LoadSceneDelayed("Game"));
+        }
+
+        public void LoadCoinPack()
+        {
+            SfxPlayer.instance.PlaySfx(0);
+            StartCoroutine(LoadSceneDelayed("BuyCoinPack"));
+        }
+
+        public void LoadMenu()
+        {
+            SfxPlayer.instance.PlaySfx(0);
+            StartCoroutine(LoadSceneDelayed("Menu"));
+        }
+
+        public IEnumerator LoadSceneDelayed(string sceneToLead, float delay = 0.25f)
+        {
+            yield return new WaitForSeconds(delay);
+            SceneManager.LoadScene(sceneToLead);
+        }
+
+        public void Pause()
+        {
+            SfxPlayer.instance.PlaySfx(0);
+            PauseManager.instance.PauseGame();
+
+            //use new UI
+            DisplayPausePanel();
+        }
+
+        public void Unpause()
+        {
+            SfxPlayer.instance.PlaySfx(0);
+            PauseManager.instance.UnPauseGame();
+
+            //use new UI
+            HidePausePanel();
+        }
+
+        public void Restart()
+        {
+            SfxPlayer.instance.PlaySfx(0);
+
+            Unpause();
+            SceneManager.LoadScene("Game");
+        }
+
+        public void LoadMenuFromGameScene()
+        {
+            SfxPlayer.instance.PlaySfx(0);
+
+            Unpause();
+            SceneManager.LoadScene("Menu");
+        }
+        public void LoadMenuFromPackingScene() //추가
+        {
+            Debug.Log("Returning to Menu from Packing scene...");
+        
+            // SceneManager를 사용해 Menu 씬 로드
+            try
+            {
+                SceneManager.LoadScene("Menu"); // "Menu"는 메뉴 씬 이름 (Build Settings에서 확인)
+            }
+            catch
+            {
+                Debug.LogError("Menu scene could not be loaded. Check if it is added in Build Settings.");
+            }
+        }
+        public void DisplayPausePanel()
+        {
+            GameObject pausePanel = GameObject.FindGameObjectWithTag("PausePanelUI");
+            pausePanel.transform.GetChild(0).gameObject.SetActive(true);
+        }
+
+        public void HidePausePanel()
+        {
+            GameObject pausePanel = GameObject.FindGameObjectWithTag("PausePanelUI");
+            pausePanel.transform.GetChild(0).gameObject.SetActive(false);
+        }
+
+    }
+
+    
+}
